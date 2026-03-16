343a3a6df0ee8277f611d2705574766c29103666
3Jlou <uranov2017@gmail.com>
2026-03-17 02:21:45 +0300

    ааазах, я ъьу хуйню удалили 2 раза
    но 1 раз восстановил через пакет, а 2 раз по номеру

--- n3.c
+++ n3.c
@@ -4,27 +4,3 @@ int proverka(int a)
 {
 	return a % 2 == 0;
 }
-
-
-// вспомогательные ф/и для 14 и 24
-static void ms_merge(int* a, int* buf, int l, int m, int r)
-{
-    int i = l, j = m + 1, k = l;
-    while (i <= m && j <= r)
-        buf[k++] = (a[i] <= a[j]) ? a[i++] : a[j++];
-    while (i <= m) buf[k++] = a[i++];
-    while (j <= r) buf[k++] = a[j++];
-    memcpy(a + l, buf + l, (size_t)(r - l + 1) * sizeof(int));
-}
-
-static void ms_rec(int* a, int* buf, int l, int r)
-{
-    if (l >= r) return;
-    int m = l + (r - l) / 2;
-    ms_rec(a, buf, l, m);
-    ms_rec(a, buf, m + 1, r);
-    ms_merge(a, buf, l, m, r);
-}
-
-
-
